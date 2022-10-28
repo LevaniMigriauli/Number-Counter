c15055ace7d2c982d74af298cba25c7323cf6592
LevaniMigriauli <levanimigriauli16@gmail.com>
2022-10-28 11:09:04 +0400

    color functionality renewed

--- script.js
+++ script.js
@@ -5,10 +5,19 @@ let btnDecrease = document.querySelector(".btn-decrease");
 let btnReset = document.querySelector(".btn-reset");
 let btnIncrease = document.querySelector(".btn-increase");
 
-btnDecrease.addEventListener("click", function () {
+btnDecrease.addEventListener("click", () => {
   counterNumber.innerText--;
+  if (counterNumber.innerText < 0) counterNumber.style.color = "red";
+  if (counterNumber.innerText == 0) counterNumber.style.color = "black";
 });
 
-btnReset.addEventListener("click", () => (counterNumber.textContent = 0));
+btnReset.addEventListener("click", () => {
+  counterNumber.textContent = 0;
+  counterNumber.style.color = "#000";
+});
 
-btnIncrease.addEventListener("click", () => counterNumber.textContent++);
+btnIncrease.addEventListener("click", () => {
+  counterNumber.textContent++;
+  if (counterNumber.innerText > 0) counterNumber.style.color = "green";
+  if (counterNumber.innerText == 0) counterNumber.style.color = "black";
+});
